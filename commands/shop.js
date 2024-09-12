de174d82a65ca0c4dc1617485759aeb1129514c1
kenjibailly <kenjibailly@gmail.com>
2024-09-12 21:39:30 +0000

    Added prices to shop command

--- commands/shop.js
+++ commands/shop.js
@@ -5,17 +5,33 @@ const Rewards = require('../models/rewards');
 
 async function handleShopCommand(interaction, client) {
     let rewards_options = "";
+    let tokenEmoji;
     try {
+
+        // Fetch the token emoji
+        tokenEmoji = await getTokenEmoji(interaction.guild.id);
+
+        // Check if we got an embed back instead of token emoji data
+        if (tokenEmoji && tokenEmoji.type === 'error') {
+            return {
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    embeds: [tokenEmoji], // Return the error embed
+                }
+            };
+        }
+
         const rewards = await Rewards.find();
         rewards.forEach(reward => {
             if (reward.enable === true) {
-                rewards_options += `- **${reward.description}**\n`;
+                rewards_options += `- **${reward.description}** - **${reward.price}** ${tokenEmoji.token_emoji}\n`;
             }
         });
     } catch (error) {
         const title = "Error Rewards";
         const description = `I could not find the rewards in the database. Pleae contact the administrator.`;
-        const embed = createEmbed(title, description, "");
+        const color = "error";
+        const embed = createEmbed(title, description, color);
         return {
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
@@ -29,18 +45,6 @@ async function handleShopCommand(interaction, client) {
     const embed = createEmbed(title, description, "");
 
     try {
-        // Fetch the token emoji
-        const tokenEmoji = await getTokenEmoji(interaction.guild.id);
-
-        // Check if we got an embed back instead of token emoji data
-        if (tokenEmoji && tokenEmoji.type === 'error') {
-            return {
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    embeds: [tokenEmoji], // Return the error embed
-                }
-            };
-        }
 
         // Use tokenEmoji from the result
         const tokenEmojiName = tokenEmoji.token_emoji_name;
@@ -76,7 +80,7 @@ async function handleShopCommand(interaction, client) {
 
         const errorTitle = "Error";
         const errorDescription = "An error occurred while processing the shop command. Please try again later.";
-        const errorColor = "#ff0000";
+        const errorColor = "error";
         const errorEmbed = createEmbed(errorTitle, errorDescription, errorColor);
 
         return {
