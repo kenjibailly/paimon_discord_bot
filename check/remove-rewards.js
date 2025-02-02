776a2e231cdd30b8bbf23b6ade411fb93f6b8cb9
kenjibailly <kenjibailly@gmail.com>
2025-02-02 20:19:43 +0000

    bug fix

--- check/remove-rewards.js
+++ check/remove-rewards.js
@@ -1,118 +1,119 @@
-const removeNickname = require('./remove-reward/remove-nickname');
-const removeCustomEmoji = require('./remove-reward/remove-custom-emoji');
-const removeCustomRole = require('./remove-reward/remove-custom-role');
-const removeCustomChannel = require('./remove-reward/remove-custom-channel');
-const createEmbed = require('../helpers/embed');
-const AwardedReward = require('../models/awarded-reward');
-const Rewards = require('../models/rewards');
-
+const removeNickname = require("./remove-reward/remove-nickname");
+const removeCustomEmoji = require("./remove-reward/remove-custom-emoji");
+const removeCustomRole = require("./remove-reward/remove-custom-role");
+const removeCustomChannel = require("./remove-reward/remove-custom-channel");
+const createEmbed = require("../helpers/embed");
+const AwardedReward = require("../models/awarded-reward");
+const Rewards = require("../models/rewards");
 
 async function checkRemoveRewards(client) {
-    const old_rewards = await findOldRewards();
-    for (const reward of old_rewards) {
-        switch (reward.reward) {
-            case "change-own-nickname":
-                await removeNickname(client, reward);
-                await removeReward(client, reward);
-                break;
-            case "change-user-nickname":
-                await removeNickname(client, reward);
-                await removeReward(client, reward);
-                break;
-            case "custom-emoji":
-                await removeCustomEmoji(client, reward);
-                await removeReward(client, reward);
-                break;
-            case "custom-role":
-                await removeCustomRole(client, reward);
-                await removeReward(client, reward);
-                break;
-            case "custom-channel":
-                await removeCustomChannel(client, reward);
-                await removeReward(client, reward);
-                break;
-            default:
-                break;
-        }
+  const old_rewards = await findOldRewards();
+  for (const reward of old_rewards) {
+    switch (reward.reward) {
+      case "change-own-nickname":
+        await removeNickname(client, reward);
+        await removeReward(client, reward);
+        break;
+      case "change-user-nickname":
+        await removeNickname(client, reward);
+        await removeReward(client, reward);
+        break;
+      case "custom-emoji":
+        await removeCustomEmoji(client, reward);
+        await removeReward(client, reward);
+        break;
+      case "custom-role":
+        await removeCustomRole(client, reward);
+        await removeReward(client, reward);
+        break;
+      case "custom-channel":
+        await removeCustomChannel(client, reward);
+        await removeReward(client, reward);
+        break;
+      default:
+        break;
     }
+  }
 }
 
 async function removeReward(client, reward) {
-    try {
-        // Await the findOneAndDelete operation
-        const removed_reward = await AwardedReward.findOneAndDelete({ _id: reward._id });
-        if (removed_reward) {
-            logger.success("Reward removed from database");
-        } else {
-            logger.error("No reward found with ID", reward._id);
-        }
-    } catch (error) {
-        logger.error('Error removing reward from database:', error);
-
-        const title = "Award Reset Error";
-        const description = `I could not remove reward with id \`${reward._id}\`, please contact your administrator.`;
-        const color = "error";
-        const embed = createEmbed(title, description, color);
-
-        try {
-            // Fetch the guild (server) using the guild ID
-            const guild = await client.guilds.fetch(reward.guild_id);
-        
-            // Fetch the owner of the guild
-            const owner = await guild.fetchOwner();
-        
-            // Send the embed as a direct message (DM) to the owner
-            await owner.send({
-                embeds: [embed],
-            });
-        
-            logger.success('Message sent to the server owner successfully.');
-        } catch (error) {
-            logger.error('Error sending message to the server owner:', error);
-        }
+  try {
+    // Await the findOneAndDelete operation
+    const removed_reward = await AwardedReward.findOneAndDelete({
+      _id: reward._id,
+    });
+    if (removed_reward) {
+      logger.success("Reward removed from database");
+    } else {
+      logger.error("No reward found with ID", reward._id);
     }
+  } catch (error) {
+    logger.error("Error removing reward from database:", error);
+
+    const title = "Award Reset Error";
+    const description = `I could not remove reward with id \`${reward._id}\`, please contact your administrator.`;
+    const color = "error";
+    const embed = createEmbed(title, description, color);
+
+    try {
+      // Fetch the guild (server) using the guild ID
+      const guild = await client.guilds.fetch(reward.guild_id);
+
+      // Fetch the owner of the guild
+      const owner = await guild.fetchOwner();
+
+      // Send the embed as a direct message (DM) to the owner
+      await owner.send({
+        embeds: [embed],
+      });
+
+      logger.success("Message sent to the server owner successfully.");
+    } catch (error) {
+      logger.error("Error sending message to the server owner:", error);
+    }
+  }
 }
 
 async function findOldRewards(guildId) {
-    let rewardsMap = new Map();
+  let rewardsMap = new Map();
 
-    try {
-        // Fetch all rewards
-        const rewards = await Rewards.find();
+  try {
+    // Fetch all rewards
+    const rewards = await Rewards.find();
 
-        // Map each reward to its time value
-        rewards.forEach(reward => {
-            rewardsMap.set(reward.name, reward.time); // Use reward.name or another unique identifier
-        });
+    // Map each reward to its time value
+    rewards.forEach((reward) => {
+      rewardsMap.set(reward.name, reward.time); // Use reward.name or another unique identifier
+    });
 
-        // Initialize array to hold old awarded rewards
-        let oldAwardedRewards = [];
+    // Initialize array to hold old awarded rewards
+    let oldAwardedRewards = [];
 
-        // Fetch all awarded rewards
-        const awardedRewards = await AwardedReward.find({ guild_id: guildId });
+    // Fetch all awarded rewards
+    const awardedRewards = await AwardedReward.find();
 
-        for (const awardedReward of awardedRewards) {
-            const rewardTime = rewardsMap.get(awardedReward.reward);
-            if (rewardTime !== undefined) {
-                // Calculate the date based on the reward's time value
-                const rewardDate = new Date();
-                rewardDate.setDate(rewardDate.getDate() - rewardTime);
+    for (const awardedReward of awardedRewards) {
+      const rewardTime = rewardsMap.get(awardedReward.reward);
+      if (rewardTime !== undefined) {
+        // Calculate the date based on the reward's time value
+        const rewardDate = new Date();
+        rewardDate.setDate(rewardDate.getDate() - rewardTime);
 
-                // Check if the awardedReward's date is older than the calculated date
-                if (awardedReward.date < rewardDate) {
-                    oldAwardedRewards.push({
-                        ...awardedReward.toObject(), // Convert Mongoose document to plain object
-                        time: rewardTime
-                    });
-                }
-            }
+        // Check if the awardedReward's date is older than the calculated date
+        if (awardedReward.date < rewardDate) {
+          oldAwardedRewards.push({
+            ...awardedReward.toObject(), // Convert Mongoose document to plain object
+            time: rewardTime,
+          });
         }
-
-        return oldAwardedRewards;
-    } catch (error) {
-        logger.error('Error finding old rewards:', error);
-        // Handle error appropriately
+      }
     }
+
+    return oldAwardedRewards;
+  } catch (error) {
+    logger.error("Error finding old rewards:", error);
+    // Handle error appropriately
+  }
 }
 
 module.exports = checkRemoveRewards;
